68d8979c16648c02efe2ba2d0fd69c6a6bfbbc7f
md-alif007 <mohammadalif043@gmail.com>
2025-02-09 17:32:29 +0600

    finding the perfect square number

---
+++ perfectSquare.js
@@ -0,0 +1,12 @@
+const num = 64;
+
+const numSquareRoot = Math.sqrt(64);
+console.log(numSquareRoot);
+
+if (Number.isInteger(numSquareRoot)) {
+    console.log(num + ' It is a perfect square');
+
+} else {
+    console.log(num + ' It is not perfect square.');
+
+}
