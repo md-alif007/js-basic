51071607c1becd3bad5d005e20b08ce0991bf07f
md-alif007 <mohammadalif043@gmail.com>
2025-02-10 11:49:41 +0600

    new code for star pattern

---
+++ starPattern/invertedRightTri.js
@@ -0,0 +1,11 @@
+for (let i = 5; i <= 1; i++) {
+
+    let row = '';
+
+    for(let j = i; j <= 1; j++){
+        row += '*';
+    }   
+
+    console.log(row);
+    
+}
--- starPattern/starPattern.js
+++ starPattern/starPattern.js
@@ -1,11 +1,4 @@
-for (let i = 1; i <= 5; i++) {
-
-    let row = '';
-
-    for(let j = 1; j <= i; j++){
-        row += '*';
-    }   
-
-    console.log(row);
-    
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    console.log('*'.repeat(i));
 }
