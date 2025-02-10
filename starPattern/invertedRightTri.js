030d6d8b0ce5dc480270b7a168bb6fa084f0d8f8
md-alif007 <mohammadalif043@gmail.com>
2025-02-10 11:58:34 +0600

    inverted right triangle print

--- starPattern/invertedRightTri.js
+++ starPattern/invertedRightTri.js
@@ -1,11 +1,5 @@
-for (let i = 5; i <= 1; i++) {
+let n = 5;
 
-    let row = '';
-
-    for(let j = i; j <= 1; j++){
-        row += '*';
-    }   
-
-    console.log(row);
-    
+for( let i = n; i >= 1; i--){
+    console.log('*'.repeat(i));
 }
